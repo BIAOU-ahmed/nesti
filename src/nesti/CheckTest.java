7b1dba07ea830898b444826242c2d07362aad92f
Ahmed <ahmed.alidjinou@hotmail.com>
2020-12-14 14:59:59 +0100

    add of unit test

---
+++ src/nesti/CheckTest.java
@@ -0,0 +1,28 @@
+package nesti;
+
+import junit.framework.TestCase;
+
+public class CheckTest extends TestCase {
+
+	
+
+	public void testCheckPassword() {
+		
+		assertEquals(0, Check.calculatePasswordStrength(""));
+		assertEquals(0, Check.calculatePasswordStrength("qsdq"));
+		assertEquals(0, Check.calculatePasswordStrength("aZ1"));
+		assertEquals(5, Check.calculatePasswordStrength("azerty1a"));
+		assertEquals(7, Check.calculatePasswordStrength("azerty1A"));
+		assertEquals(9, Check.calculatePasswordStrength("Azerty1!"));
+		assertEquals(10, Check.calculatePasswordStrength("Azerty1!azer"));
+	}
+	public void testCheckEmail() {
+		
+		assertEquals(false, Check.isValidEmail(""));
+		assertEquals(false, Check.isValidEmail("ahmed"));
+		assertEquals(false, Check.isValidEmail("ahmed@example"));
+		assertEquals(false, Check.isValidEmail("ahmed@example.c"));
+		assertEquals(true, Check.isValidEmail("ahmed@example.com"));
+	}
+	
+}
